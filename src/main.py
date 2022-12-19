bef3f1734c9fb978228d514225526ad77727cb00
ustato <atsuto.fps@gmail.com>
2022-12-19 22:17:55 +0900

    ファイルアップロードを追加

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 """FastAPIメインクラス."""
 
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, UploadFile
 from fastapi.security import OAuth2PasswordRequestForm
 
 from models.token_model import TokenResponse
 from models.user_model import UserResponse
-from services.user_service import get_current_active_user, login_for_access_token
+from services import file_service, user_service
 
 app = FastAPI()
 
@@ -15,13 +15,22 @@ app = FastAPI()
 def token(
     form_data: OAuth2PasswordRequestForm = Depends(),
 ):
-    """アクセストークン発行エンドポイント."""
-    return login_for_access_token(form_data)
+    """アクセストークン発行API."""
+    return user_service.login_for_access_token(form_data)
 
 
-@app.get("/users/me/", response_model=UserResponse)
+@app.get("/users/me", response_model=UserResponse)
 def read_users_me(
-    current_user: UserResponse = Depends(get_current_active_user),
+    current_user: UserResponse = Depends(user_service.get_current_active_user),
 ):
-    """ユーザ情報確認用エンドポイント."""
+    """ユーザ情報確認用API."""
     return current_user
+
+
+@app.post("/statistics")
+def csv_statistics(
+    upload_file: UploadFile,
+    current_user: UserResponse = Depends(user_service.get_current_active_user),
+):
+    """アップロードされたCSVのそれぞれのカラムの統計量を算出して、結果をJSONで返すAPI."""
+    return file_service.aggregate_csv(upload_file.file)
